bc54932e45f841e126d28c7bfbd8ad90eb8d2611
Houtamelo <antoniopedrogf@hotmail.com>
2026-06-26 23:53:06 -0300

    Make Cow Arg public

--- godot-core/src/meta/args/mod.rs
+++ godot-core/src/meta/args/mod.rs
@@ -22,10 +22,6 @@ pub use as_arg::{
     ArgPassing, AsArg, AsDirectElement, ByObject, ByOption, ByRef, ByValue, ByVariant, ToArg,
     owned_into_arg, ref_to_arg,
 };
-#[cfg(not(feature = "trace"))]
-pub(crate) use cow_arg::{CowArg, FfiArg};
-// Integration test only.
-#[cfg(feature = "trace")]
 #[doc(hidden)]
 pub use cow_arg::{CowArg, FfiArg};
 pub use object_arg::ObjectArg;
